9351f1fa7138a6e71477e53a02de078c6524f4fb
YanLinMyintt <yuko699468@gmai.com>
2020-11-30 20:44:59 +0630

    first commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-$(document).ready(function (){
-	$win = $(window);
-	$set = $('#all-movies .title');
-	$btn = $('.fa-cog');
 
-	
-	$win.resize(function (){
-		if($win.width() <= 768){
-			$set.hide();
-					
-		}else{
-			$times.hide();
-			$sidebar.show();
-			$bars.hide();
-		}
-	})
-
-})
 
 
